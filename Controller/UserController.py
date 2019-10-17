7eb5d75c6ce86b110fdc0e021a82a959be038067
patobengoechea <pato.bengoechea@gmail.com>
2019-10-17 17:41:12 -0300

    getUser

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -1,5 +1,12 @@
-
+from TPFinalPython.DataModel.UserData import UserData
 
 
 class UserController:
-    #Controlador de usuario
+
+    def createUser(self, name, surname, user, password, wallet):
+        dbUser = UserData()
+        dbUser.createUser(name, surname, user, password, wallet)
+        print("se creo un usuario")
+
+
+
--- DataModel/UserData.py
+++ DataModel/UserData.py
@@ -1,6 +1,9 @@
-from Database import Database
+from TPFinalPython.DataModel.Database import Database
+from TPFinalPython.Model.User import User
 
 class UserData:
+
+
 
     def createUser(self, name, surname, user, password, wallet):
         db = Database()
@@ -12,8 +15,19 @@ class UserData:
             'password': password,
             'wallet': wallet
         }
-        cursor.usuario.insert(newUser)
+        cursor.usuario.insert_one(newUser)
 
 
-ud = UserData()
-ud.createUser("pato", "ben", "pb", "1995", 1000)
+    def getUser(self, user, password):
+
+        #Hacer try catch, que pasa si no encuentra un usuario?
+        db = Database()
+        cursor = db.main()
+        logUserJSON = cursor.usuario.find_one({"user": user})
+        logUserModel = User()
+        logUserModel.user = logUserJSON["user"]
+
+        print(logUserModel.user)
+
+a = UserData()
+a.getUser('pb', 112)
